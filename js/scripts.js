98ec36f8220236949aa47e187059a67e5279c5f4
reenatt-dotcom <rtthomas@ucsc.edu>
2023-06-23 23:19:10 -0700

    update

--- js/scripts.js
+++ js/scripts.js
@@ -55,3 +55,8 @@ window.addEventListener('DOMContentLoaded', event => {
 
 
 
+window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName('form')) {
+      form.reset();
+    }
+  }
